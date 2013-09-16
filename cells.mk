829b6df56a83f6c871f996c75d0877792b684b63
Jeremy C. Andrus <jeremya@cs.columbia.edu>
2013-09-16 15:17:30 -0400

    cells: custom init.rc files for host and cell
    Tell the Android build system that we provide a custom init.rc file.
    
    The Cells init.rc has been trimmed down to support a minimal
    command-line interface for the host. The init.cell.rc file contains an
    init.rc file suitable for starting a guest Cell. It is the
    responsibility of CellD to manage cell startup and use of the proper
    init file through a custom hook into the Android init process.
    
    Change-Id: Id42d895d4192e5609e7a66380803d232a447ee9b
    Signed-off-by: Jeremy C. Andrus <jeremya@cs.columbia.edu>

--- cells.mk
+++ cells.mk
@@ -23,5 +23,13 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 PRODUCT_PROPERTY_OVERRIDES += \
 	persist.security.efs.enabled=0
 
+# prevent the default init.rc from being used
+TARGET_PROVIDES_INIT_RC := true
+
+# This is the init.rc used by cells
+PRODUCT_COPY_FILES += \
+	vendor/columbia/build/init.rc:root/init.rc \
+	vendor/columbia/build/init.cell.rc:root/init.cell.rc
+
 # inherit from the proprietary Google apps target, if present
 $(call inherit-product-if-exists, vendor/columbia/external/gapps/gapps.mk)
--- grouper/device-vendor.mk
+++ grouper/device-vendor.mk
@@ -2,3 +2,6 @@
 include vendor/columbia/build/cells.mk
 
 # include any device-dependent files / packages here...
+PRODUCT_COPY_FILES += \
+	vendor/columbia/build/grouper/init.grouper.cell.rc:root/init.grouper.cell.rc \
+	vendor/columbia/build/grouper/init.grouper.rc:root/init.grouper.rc
